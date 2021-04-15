e7dcfd005b463a046711156386051c2c2aef7c9b
saragylden <76065168+saragylden@users.noreply.github.com>
2021-04-15 15:58:24 +0200

    JS - Extrafun
    Der er lidt bøvl med headeren, udover det så er den sticky - kigger stadig å det

---
+++ extrafun.js
@@ -0,0 +1,15 @@
+// Sticky navigation bar - Sara //
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
