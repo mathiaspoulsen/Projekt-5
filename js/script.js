02924671d6932b9e3e1c6f825550be83c9e0319a
Emeliesteg123 <emelie.1999@live.dk>
2021-04-16 16:33:11 +0200

    Light and dark mode

--- js/script.js
+++ js/script.js
@@ -122,3 +122,26 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+var checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function() {
+  if(this.checked) {
+    trans()
+    document.documentElement.setAttribute('data-theme', 'dark')
+    
+  } else {
+    trans()
+    document.documentElement.setAttribute('data-theme', 'light')
+    
+  }
+})
+
+let trans = () => {
+  document.documentElement.classList.add('transition');
+  window.setTimeout(() => {
+    document.documentElement.classList.remove('transition');
+    
+  }, 1000)
+}
